62b2f4993611f20b1bea9501e24ad75ad2983395
Band-Aid <21311546+Band-Aid@users.noreply.github.com>
2024-11-20 16:49:21 +0900

    Initial Commit

---
+++ swiftyapp/swiftyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftyappApp.swift
+//  swiftyapp
+//
+//  Created by Daichi Ishida on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftyappTests/swiftyappTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftyappTests.swift
+//  swiftyappTests
+//
+//  Created by Daichi Ishida on 2024/11/20.
+//
+
+import Testing
+@testable import swiftyapp
+
+struct swiftyappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
